ad38b662071069eb6a825c3b6892ae3d99838656
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-01-09 15:14:54 +0000

    add disable-* options, use testing framework

--- lint.js
+++ lint.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
-function lint(filename, options = {}) {
+function lint(filename, options = []) {
     let n = JSON.parse(fs.readFileSync(filename));
     for (var i in n.cells) {
-        if (options.output && n.cells[i].hasOwnProperty('output')) {
+        if (!options.includes('--disable-cells') && n.cells[i].hasOwnProperty('output')) {
             return false;
         }
-        if (options.execution_count && n.cells[i].execution_count != null) {
+        if (options.includes('--disable-counts') && n.cells[i].execution_count != null) {
             return false;
         }
     }
     return true;
 }
+
+module.exports = lint;
