eec41738f1dc2e7740de2e24c7e9b6dd91fc07dc
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-01-09 15:42:53 +0000

    add logging for linting errors

--- lint.js
+++ lint.js
@@ -4,9 +4,11 @@ function lint(filename, options = []) {
     let n = JSON.parse(fs.readFileSync(filename));
     for (var i in n.cells) {
         if (!options.includes('--disable-cells') && n.cells[i].output) {
+            console.log(`${filename}: output found`);
             return false;
         }
         if (!options.includes('--disable-counts') && n.cells[i].execution_count != null) {
+            console.log(`${filename}: execution count found`);
             return false;
         }
     }
