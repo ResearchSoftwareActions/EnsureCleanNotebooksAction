656d4655c80faf11478bd7cdfa9d5722be873d9e
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-01-09 15:36:18 +0000

    add linting, do not use hasOwnProperty

--- lint.js
+++ lint.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function lint(filename, options = []) {
     let n = JSON.parse(fs.readFileSync(filename));
     for (var i in n.cells) {
-        if (!options.includes('--disable-cells') && n.cells[i].hasOwnProperty('output')) {
+        if (!options.includes('--disable-cells') && n.cells[i].output) {
             return false;
         }
         if (!options.includes('--disable-counts') && n.cells[i].execution_count != null) {
