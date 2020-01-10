459e9b946c7fac045f2711a292cecee0203d2874
Abhishek Dasgupta <abhishek.dasgupta@cs.ox.ac.uk>
2020-01-10 10:20:30 +0000

    Add parsing of input from actions/core

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-//const core = require('@actions/core');
+const core = require('@actions/core');
 //const github = require('@actions/github');
 const walk = require('walk');
 const path = require('path');
 const lint = require('./lint');
 
+const disableChecks = core.getInput('disable-checks', { required: false });
+var lintOptions = [];
+if (disableChecks) {
+    lintOptions = disableChecks.split(',');
+}
+
 var walker = walk.walk(".", {followLinks: false, filters: ["node_modules"]});
 var results = [];
 walker.on("file", function (root, fileStats, next) {
   if (path.extname(fileStats.name) == '.ipynb') {
-      results.push(lint(path.join(root, fileStats.name)));
+      results.push(lint(path.join(root, fileStats.name), lintOptions));
   }
   next();
 });
 walker.on("end", function() {
     console.log(results);
     if(!results.every(i => i)) {
-        process.exit(1);
+          core.setFailed('Lint failed');
     }
 });
 
--- lint.js
+++ lint.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 function lint(filename, options = []) {
     let n = JSON.parse(fs.readFileSync(filename));
     for (var i in n.cells) {
-        if (!options.includes('--disable-cells') && n.cells[i].output) {
+        if (!options.includes('outputs') && n.cells[i].output) {
             console.log(`${filename}: output found`);
             return false;
         }
-        if (!options.includes('--disable-counts') && n.cells[i].execution_count != null) {
+        if (!options.includes('execution-counts') && n.cells[i].execution_count != null) {
             console.log(`${filename}: execution count found`);
             return false;
         }
